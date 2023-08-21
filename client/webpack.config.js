e35482da00f03f4aa24a77bb208b0e15c1d3436a
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-21 14:54:30 -0500

    testing stage

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,7 +29,19 @@ module.exports = () => {
       }),
       new WebpackPwaManifest({
         name:'Just Another Text Editor',
-        
+        short_name: 'JATE',
+        description: 'just another text editor of course',
+        background_color:'#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath:'/',
+        icons:[
+          {
+            src:path.resolve('src/images/logo.png'),
+            sizes: [96, 129, 256, 384, 512],
+            destination: path.join('assets','icons'),
+          }
+        ]
 
       })
 
@@ -37,7 +49,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test:/\.css$/i,
+          use:['style-loader','css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options:{
+              presets: ['@babel/prest-env'],
+            plugins:['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
